d6f03fe471b940ee1f35bc5334851432fc5158ef
PangeranLord <98150373+PangeranLord@users.noreply.github.com>
2022-04-04 05:57:01 +0700

    Update sc.js

--- plugins/sc.js
+++ plugins/sc.js
@@ -1,4 +1,4 @@
-let handler = async (m, { conn }) => m.reply('https://github.com/RTeam1/RFK-Bot')
+let handler = async (m, { conn }) => m.reply('https://instagram.com/pangerann21_\nFollowing IG Owner, Klik Link Di Atas!')
 handler.help = ['script', 'sc']
 handler.tags = ['info']
 handler.command = /^s(c|cript)$/i
