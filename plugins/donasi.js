a1e5f401863453042632193661023fc162bc348b
PangeranLord <98150373+PangeranLord@users.noreply.github.com>
2022-04-04 05:33:44 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -2,11 +2,11 @@ let fetch = require('node-fetch')
 let handler = async (m, { conn }) => conn.sendButtonLoc(m.chat, await (await fetch(fla + 'donasi')).buffer(), `
 
 ┌〔 Donasi • Emoney 〕
-├ Pulsa : 0896-7776-3976
-├ OVO : 0896-7776-3976
-├ Dana : 0896-7776-3976
-├ Gopay : 0896-7776-3976
-└────
+├ *Pulsa* : 0878-3214-7584
+├ *OVO* : 0878-3214-7584
+├ *Dana* : 0878-3214-7584
+├ *Gopay* : 0878-3214-7584
+└〔 $namabot 〕
 `.trim(), footer, 'Owner', '.owner')
 handler.help = ['donasi']
 handler.tags = ['info']
