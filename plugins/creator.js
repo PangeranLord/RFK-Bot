c0ee541091fa78c66ddc73e60aa334e877ef8328
PangeranLord <98150373+PangeranLord@users.noreply.github.com>
2022-04-04 13:26:02 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,81 +1,14 @@
-const { MessageType } = require('@adiwajshing/baileys')
-const PhoneNumber = require('awesome-phonenumber')
-async function handler(m) {
-  let name = 'Koko Pangeran'
-  number = owner[0].replace(/[^0-9]/g, '')
-  let njid = number + '@s.whatsapp.net'
-  let onW = await this.isOnWhatsApp(njid) || { isBusiness: false }
-
-  let name2 = 'Nomor Bot Pangeran'
-  number2 = owner[1].replace(/[^0-9]/g, '')
-  let njid2 = number2 + '@s.whatsapp.net'
-  let onW2 = await this.isOnWhatsApp(njid2) || { isBusiness: false }
-
-  let name3 = 'Koko Pangeran'
-  number3 = owner[2].replace(/[^0-9]/g, '')
-  let njid3 = number3 + '@s.whatsapp.net'
-  let onW3 = await this.isOnWhatsApp(njid3) || { isBusiness: false }
-
-let name4 = 'Cece Gntg'
-  number4 = owner[3].replace(/[^0-9]/g, '')
-  let njid4 = number4 + '@s.whatsapp.net'
-  let onW4 = await this.isOnWhatsApp(njid4) || { isBusiness: false }
-
-  m.reply(`Itu Nomor Ownerku Paling _*Ganteng/Cantik*_ Kak!\n\nSilahkan Chat Jika Ada Keperluan.\nTidak Menerima Chat "P" Utamakan Salam ya Sayang❤️`)
-  this.sendMessage(m.chat, {
-    contacts: [{
-      displayname: name, vcard: `
-BEGIN:VCARD
-VERSION:3.0
-N:;${name.replace(/\n/g, '\\n')};;;
-FN:${name.replace(/\n/g, '\\n')}
-TEL;type=CELL;type=VOICE;waid=${number}:${PhoneNumber('+' + number).getNumber('international')}${onW.isBusiness ? `
-X-WA-BIZ-NAME:${(this.contacts[njid].vname || this.getName(njid)).replace(/\n/, '\\n')}
-X-WA-BIZ-DESCRIPTION:${((await this.getBusinessProfile(njid)).description || '').replace(/\n/g, '\\n')}
-` : ''}
-END:VCARD
-`.trim()
-    }, {
-      displayname: name2, vcard: `
-BEGIN:VCARD
-VERSION:3.0
-N:;${name2.replace(/\n/g, '\\n')};;;
-FN:${name2.replace(/\n/g, '\\n')}
-TEL;type=CELL;type=VOICE;waid=${number2}:${PhoneNumber('+' + number2).getNumber('international')}${onW2.isBusiness ? `
-X-WA-BIZ-NAME:${(this.contacts[njid2].vname || this.getName(njid2)).replace(/\n/, '\\n')}
-X-WA-BIZ-DESCRIPTION:${((await this.getBusinessProfile(njid2)).description || '').replace(/\n/g, '\\n')}
-` : ''}
-END:VCARD
-`.trim()
-    }, {displayname: name3, vcard: `
-BEGIN:VCARD
-VERSION:3.0
-N:;${name3.replace(/\n/g, '\\n')};;;
-FN:${name3.replace(/\n/g, '\\n')}
-TEL;type=CELL;type=VOICE;waid=${number3}:${PhoneNumber('+' + number3).getNumber('international')}${onW3.isBusiness ? `
-X-WA-BIZ-NAME:${(this.contacts[njid3].vname || this.getName(njid3)).replace(/\n/, '\\n')}
-X-WA-BIZ-DESCRIPTION:${((await this.getBusinessProfile(njid3)).description || '').replace(/\n/g, '\\n')}
-` : ''}
-END:VCARD
-`.trim()
-    }, {
-      displayname: name4, vcard: `
-BEGIN:VCARD
-VERSION:3.0
-N:;${name4.replace(/\n/g, '\\n')};;;
-FN:${name4.replace(/\n/g, '\\n')}
-TEL;type=CELL;type=VOICE;waid=${number4}:${PhoneNumber('+' + number4).getNumber('international')}${onW4.isBusiness ? `
-X-WA-BIZ-NAME:${(this.contacts[njid4].vname || this.getName(njid4)).replace(/\n/, '\\n')}
-X-WA-BIZ-DESCRIPTION:${((await this.getBusinessProfile(njid4)).description || '').replace(/\n/g, '\\n')}
-` : ''}
-END:VCARD
-`.trim()
-    }]
-  }, MessageType.contactsArray, { quoted: m })
+function handler(m) {
+   let listOwner = new Array()
+  for (let i of owner.map(v => v.replace(/\D/g, '') + '@s.whatsapp.net')) {
+  	listOwner.push({ vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;;;;\nFN:${this.getName(i)}\nitem1.TEL;waid=${i.split('@')[0]}:${i.split('@')[0]}\nitem1.X-ABLabel:Work\nURL;Web gwejh: http://instagram.com/pangerann21_\nEMAIL;y: pangeran.dwnta23@gmail.com\nORG: BUKAN BOT + NO SAVE\nTEL;NO CHAT P ANJG;waid= ${i.split('@')[0]}:${i.split('@')[0]}\nEND:VCARD` })
+  }
+  let send = this.sendMessage(m.chat, { displayName: listOwner.length + ' kontak', contacts: listOwner }, 'contactsArrayMessage', { quoted: m })
+return m.reply('Chat owner *P & Minta Sv*\n\n *Ga ada sopan santun*')
 }
+
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
-
 handler.command = /^(nowner|creator)$/i
 
 module.exports = handler
