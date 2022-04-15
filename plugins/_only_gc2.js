77c8fffc9807fe8b89b3e6160eb932a2e76e2afc
PangeranLord <98150373+PangeranLord@users.noreply.github.com>
2022-04-15 10:12:59 +0700

    Create _only_gc2.js

---
+++ plugins/_only_gc2.js
@@ -0,0 +1,20 @@
+/*****
+WATERMARK BY Koko Pangeran
+Koko Pangeran is The Real owner of RFK
+*****/
+let handler = async (m, { conn, usedPrefix, text, isAdmin, isOwner }) => {
+
+if (!isOwner) return m.reply('Lu Owner kah Dekkk?')
+if (gconly === false) return m.reply('GC only udah mati bodo\n')
+//if (modepublic === true) return 
+gconly = false
+m.reply(`Success Mematikan Mode GC Only
+Bot akan merespon di Grup dan di pribadi Bot`)
+
+}
+handler.help = ['offgconly']
+handler.tags = ['info', 'main']
+
+handler.command = /^(offgc|offgconly)$/i
+
+module.exports = handler
