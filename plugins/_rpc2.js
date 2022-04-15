b96f22e3abb814dbe96e7647770bc5730f5545b6
PangeranLord <98150373+PangeranLord@users.noreply.github.com>
2022-04-15 10:26:42 +0700

    Create _rpc2.js

---
+++ plugins/_rpc2.js
@@ -0,0 +1,19 @@
+/*****
+WATERMARK BY Koko Pangeran
+Koko Pangeran is The Real owner of RFK
+*****/
+let handler = async (m, { conn, usedPrefix, text, isAdmin, isOwner }) => {
+
+if (!isOwner) return m.reply('Lu Owner kah Dekkk?')
+if (rpc === false) return m.reply('Auto Read pribadi udah Mati Banh🗿🚬')
+//if (autovn === true) return 
+rpc = false
+m.reply(`Sukses Mematikan Mode Auto Read pribadi Bot`)
+
+}
+handler.help = ['offreadpc']
+handler.tags = ['info', 'main']
+
+handler.command = /^(offrpc|offreadpc)$/i
+
+module.exports = handler
