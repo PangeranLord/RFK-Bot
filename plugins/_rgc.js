ef3915ccb007da0bc4df2d2984ab7c06c2e5f964
PangeranLord <98150373+PangeranLord@users.noreply.github.com>
2022-04-15 10:18:32 +0700

    Create _rgc.js

---
+++ plugins/_rgc.js
@@ -0,0 +1,19 @@
+/*****
+WATERMARK BY Koko Pangeran
+Koko Pangeran is The Real owner of RFK
+*****/
+let handler = async (m, { conn, usedPrefix, text, isAdmin, isOwner }) => {
+
+if (!isOwner) return m.reply('Lu Owner kah Dekkk?')
+if (rgc === true) return m.reply('Auto Read Grup udah Aktif Banh🗿🚬')
+//if (autovn === true) return 
+rgc = true
+m.reply(`Success Activated Mode Auto Read Group`)
+
+}
+handler.help = ['readgc']
+handler.tags = ['info', 'main']
+
+handler.command = /^(rgc|readgc)$/i
+
+module.exports = handler
