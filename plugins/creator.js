6474b03d2ba610292c8ee5fddf2ced4185c563a4
PangeranLord <98150373+PangeranLord@users.noreply.github.com>
2022-04-15 10:53:37 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,10 +1,10 @@
 function handler(m) {
    let listOwner = new Array()
   for (let i of owner.map(v => v.replace(/\D/g, '') + '@s.whatsapp.net')) {
-  	listOwner.push({ vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;;;;\nFN:${this.getName(i)}\nitem1.TEL;waid=${i.split('@')[0]}:${i.split('@')[0]}\nitem1.X-ABLabel:Work\nURL;Web gwejh: http://instagram.com/pangerann21_\nEMAIL;y: pangeran.dwnta23@gmail.com\nORG: BUKAN BOT + NO SAVE\nTEL;NO CHAT P ANJG;waid= ${i.split('@')[0]}:${i.split('@')[0]}\nEND:VCARD` })
+  	listOwner.push({ vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;;;;\nFN:${this.getName(i)}\nitem1.TEL;waid=${i.split('@')[0]}:${i.split('@')[0]}\nitem1.X-ABLabel:Work\nURL;Web gwejh: https://instagram.com/pangerann21_\nEMAIL;y: pangeran.dwnta23@gmail.com\nORG: BUKAN BOT + NO SAVE\nTEL;NO CHAT P ANJG;waid= ${i.split('@')[0]}:${i.split('@')[0]}\nEND:VCARD` })
   }
   let send = this.sendMessage(m.chat, { displayName: listOwner.length + ' kontak', contacts: listOwner }, 'contactsArrayMessage', { quoted: m })
-return m.reply('Itu No Ownku yg _*Ganteng Kak😙*_\n\n_*Tidak Menerima Chat "P" Utamakan Salam Sayang*_\n*Menerima Sv Cwe Saja💦*')
+return m.reply('Itu No Ownku yg _*Ganteng/Cantik*_ Kak😙\n\n_*Tidak Menerima Chat "P" Utamakan Salam Sayang*_\n*Jgn Cht Own Cwe Tod‼️*')
 }
 
 handler.help = ['owner', 'creator']
